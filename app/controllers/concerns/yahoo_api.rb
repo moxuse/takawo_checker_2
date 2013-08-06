e392fd6d28787ab77b190e7f05393d308fe3ca22
ogawakeisuke <ogawa@uniba.jp>
2013-08-06 16:32:01 +0900

    yahooapiクラス

---
+++ app/controllers/concerns/yahoo_api.rb
@@ -0,0 +1,26 @@
+require "net/http"
+require "uri"
+
+class YahooAPI
+  attr_reader :api_id, :query, :response
+
+  def initialize(api_id, query)
+    @api_id = api_id
+    @query = query
+  end
+
+  def send
+    uri = URI.parse("http://jlp.yahooapis.jp/KeyphraseService/V1/extract?appid=#{api_id}&sentence=#{query}&output=json")
+    @response = Net::HTTP.get_print uri
+    return self
+  end
+
+  def parse_json
+    begin
+      JSON.parse(response)
+    rescue => e
+      return false
+    end
+  end
+
+end
